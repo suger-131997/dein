40e2d6c8692ef3e66c2c729aa0d4e1735f0d41ce
suger-131997 <yuya.0313.1997@gmail.com>
2025-04-03 23:27:55 +0900

    add readme and comment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dein
+# Dein - Dependency Injection  
 
+A dependency injection library for Go that uses reflection to generate code. 
+
+# Installation
+
+```bash
+go get github.com/suger-131997/dein
+```
--- example/gen/gen.go
+++ example/gen/gen.go
@@ -21,29 +21,35 @@ var filename = flag.String("output", "dein_gen.go", "output file name")
 func main() {
 	flag.Parse()
 
+	// Create a resolver
 	r := dein.NewResolver()
 
+	// Register the providers
 	dein.Register(r, dein.Mark(dein.Bind[car.ICar, *car.Car]()))
 	dein.Register(r, dein.PE1(engine.NewEngine))
 	dein.Register(r, dein.P0(wheel.NewWheel))
 	dein.Register(r, dein.P3(car.NewCar))
 	dein.Register(r, dein.PF0[light.Light]())
 
+	// Dependency resolution and create a generator
 	gen, err := r.Resolve()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Generate code
 	data, err := gen.Generate("main")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Format code
 	data, err = imports.Process(*filename, data, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Write code to file
 	err = os.WriteFile(*filename, data, 0o644)
 	if err != nil {
 		log.Fatal(err)
